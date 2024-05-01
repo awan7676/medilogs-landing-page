dc60182f09b5c0c7a5dc2eee5aeb424c07abaff1
Abdullah Awan <abdullahawan7676@gmail.com>
2024-05-02 00:30:38 +0500

    MediLogs Landing Page

---
+++ src/components/Features.tsx
@@ -0,0 +1,141 @@
+import config from '../config/index.json';
+
+const Features = () => {
+  const { features } = config;
+  const { title, items: featuresList } = features;
+  return (
+    <div className={`py-12 bg-background`} id="features">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="mt-10">
+          <div className="bg-white">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+              <div className="mx-auto max-w-2xl lg:text-center">
+                <h1
+                  className={`w-full my-2 text-5xl font-bold leading-tight text-center `}
+                >
+                  {title}
+                </h1>
+              </div>
+              <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+                <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+                  <div className="relative pl-16">
+                    <dt className="text-base font-semibold leading-7 text-gray-900">
+                      <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-primary">
+                        <svg
+                          className="h-6 w-6 text-white"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
+                          />
+                        </svg>
+                      </div>
+                      Enhance Interoperability Across Healthcare Providers
+                    </dt>
+                    <dd className="mt-2 text-base leading-7 text-gray-600">
+                      Develop a standardized interface that allows seamless data
+                      exchange between various healthcare systems, ensuring that
+                      all stakeholders—patients, doctors, and laboratories—can
+                      access and utilize medical data efficiently.
+                    </dd>
+                  </div>
+                  <div className="relative pl-16">
+                    <dt className="text-base font-semibold leading-7 text-gray-900">
+                      <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-primary">
+                        <svg
+                          className="h-6 w-6 text-white"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+                          />
+                        </svg>
+                      </div>
+                      Increase Patient Empowerment and Engagement
+                    </dt>
+                    <dd className="mt-2 text-base leading-7 text-gray-600">
+                      Provide patients with comprehensive access to their
+                      medical records and health data, empowering them to
+                      actively participate in their healthcare decisions and
+                      manage their own health effectively.
+                    </dd>
+                  </div>
+                  <div className="relative pl-16">
+                    <dt className="text-base font-semibold leading-7 text-gray-900">
+                      <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-primary">
+                        <svg
+                          className="h-6 w-6 text-white"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+                          />
+                        </svg>
+                      </div>
+                      Facilitate Data-Driven Insights and Prognostics
+                    </dt>
+                    <dd className="mt-2 text-base leading-7 text-gray-600">
+                      Leverage advanced analytics to analyze patient data across
+                      the ecosystem, enabling healthcare professionals to derive
+                      actionable insights, predict health trends, and make
+                      evidence-based decisions that improve patient care
+                      outcomes.
+                    </dd>
+                  </div>
+                  <div className="relative pl-16">
+                    <dt className="text-base font-semibold leading-7 text-gray-900">
+                      <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-primary">
+                        <svg
+                          className="h-6 w-6 text-white"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M7.864 4.243A7.5 7.5 0 0119.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 004.5 10.5a7.464 7.464 0 01-1.15 3.993m1.989 3.559A11.209 11.209 0 008.25 10.5a3.75 3.75 0 117.5 0c0 .527-.021 1.049-.064 1.565M12 10.5a14.94 14.94 0 01-3.6 9.75m6.633-4.596a18.666 18.666 0 01-2.485 5.33"
+                          />
+                        </svg>
+                      </div>
+                      Optimize Clinical Operations through Automation
+                    </dt>
+                    <dd className="mt-2 text-base leading-7 text-gray-600">
+                      Implement automated systems for managing clinical
+                      workflows, including electronic medical records,
+                      appointment scheduling, and laboratory operations, to
+                      enhance the efficiency and accuracy of healthcare
+                      services.
+                    </dd>
+                  </div>
+                </dl>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Features;
